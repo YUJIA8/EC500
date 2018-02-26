33676dbfa6e870947e66599659acc2761bcf840b
YinzhuSU <31676438+YinzhuSU@users.noreply.github.com>
2018-02-26 15:53:13 -0500

    Test2

---
+++ Test2.py
@@ -0,0 +1,30 @@
+import unittest
+import twitterWYJ
+import subprocess
+import time
+import os
+
+
+class Test2(unittest.TestCase):
+	
+	def test_mp4(self):
+			num = 0
+			subprocess.call("python twitterWYJ.py",shell = True)
+			for file in os.listdir('.'):
+				if file.endswith('mp4'):
+					num = num + 1
+			print('The amount of ".mp4" video file you have created is ' + str(num) + '.')			
+			try:
+				self.assertEqual(num,1)
+				print('You have passed the Test2! Congratulations!')
+			except:
+				try:
+					self.assertGreater(num,1)
+					print('You have created more than one ".mpt" files! Review your code and try again!')
+				except:
+					print('You have not created any ".mp4" file! Review your code and try again!')
+
+
+	
+if __name__ == '__main__': 
+		unittest.main() 
