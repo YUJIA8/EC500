958f3265b893e721e061691a85ed17fd7313b487
YinzhuSU <31676438+YinzhuSU@users.noreply.github.com>
2018-02-26 16:10:08 -0500

    Test3

---
+++ Test3.py
@@ -0,0 +1,32 @@
+import unittest
+import twitterWYJ
+import subprocess
+import time
+import os
+
+
+class Test3(unittest.TestCase):
+	
+	def test_label(self):
+		num = 0
+		os.system("twitterWYJ.py")
+		for file in os.listdir('.'):
+			if file.endswith('txt'):
+				num = num + 1
+		print('The amount of ".txt" file you have created is ' + str(num) + '.')
+		try:
+			self.assertEqual(num,1)
+			print('You have passed the Test3! Congratulations!')
+		except:
+			try:
+				self.assertGreater(num,1)
+				print('You have created more than one ".txt" files! Review your code and try again!')
+			except:
+				print('You have not created any ".txt" file! Review your code and try again!')		
+
+
+		
+
+	
+if __name__ == '__main__': 
+		unittest.main() 
